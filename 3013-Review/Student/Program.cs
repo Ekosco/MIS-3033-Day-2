4afdbce541594ebdd9a93a9a4217ea32d67f06a6
Lizzy Kosco <Ekosco@ou.edu>
2021-01-27 12:28:12 -0600

    Rought Draft of code

--- 3013-Review/Student/Program.cs
+++ 3013-Review/Student/Program.cs
@@ -6,7 +6,45 @@ namespace Student
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = (File.ReadAllLines("studentdataRandom100Rows.csv"));
+
+            List<Student> students = new List<Student>();
+
+            Student stud = new Student();
+            stud.FirstName = "Adam";
+            stud.LastName = "Ackerman";
+            stud.FavoriteColor = "blue";
+            stud.ID = 1;
+            Console.WriteLine(stud.CreateFullName());
+
+            for(int i =1;i<lines.Length; i++)
+            {
+                string line = lines[i];
+
+                string[] pieces = line.Split(',');
+
+                //int id = Convert.ToInt32(pieces[0]);
+
+                Student curentStudent = new Student(Convert.ToInt32(pieces[0]), pieces[1], pieces[2], pieces[3]);
+
+                students.Add(curentStudent);
+            }
+
+            //output all the students whose favorite color is Puce
+            foreach(var student in students)
+            {
+                if (student.FavoriteColor.ToLower() == "puce")
+                {
+                    Console.WriteLine(student);
+                }
+            }
+
+            //foreach (var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            Console.ReadKey();
         }
     }
 }
---
+++ 3013-Review/Student/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    public class Student
+    {
+
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+//Press Alt + Shift + up/down arrows for a multitype in VS
+        
+
+        //Default/Empty Constructor for the Student Class that sets every property to its starting value 
+        public Student()
+        {
+            ID = 0;
+            FirstName = "";
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+//CTOR tab tab creates a constructor for you 
+        public Student(int id, string first, string last, string color)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+        }
+
+
+        //Example if id was the only variable called in the constructor 
+        //public Student(int id)
+        //{
+        //    ID = id;
+        //    FirstName = "";
+        //    LastName = string.Empty;
+        //    FavoriteColor = string.Empty;
+        //}
+        ////Or
+        //public Student(int id) : this()
+        //{
+        //    ID = id;
+        //}
+
+        public string CreateFullName()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+
+
+    }
+}
